e5f6b675f3a170ff7a26ef294ba5951da2b4579a
yilanglab <236250373@qq.com>
2026-03-19 20:02:45 +0800

    0319-部署问题修复

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -63,16 +63,16 @@ function Carousel({
 
   const onSelect = React.useCallback((api: CarouselApi) => {
     if (!api) return
-    setCanScrollPrev(api.canScrollPrev())
-    setCanScrollNext(api.canScrollNext())
+    setCanScrollPrev(api.canGoToPrev())
+    setCanScrollNext(api.canGoToNext())
   }, [])
 
   const scrollPrev = React.useCallback(() => {
-    api?.scrollPrev()
+    api?.goToPrev()
   }, [api])
 
   const scrollNext = React.useCallback(() => {
-    api?.scrollNext()
+    api?.goToNext()
   }, [api])
 
   const handleKeyDown = React.useCallback(
@@ -96,10 +96,11 @@ function Carousel({
   React.useEffect(() => {
     if (!api) return
     onSelect(api)
-    api.on("reInit", onSelect)
+    api.on("reinit", onSelect)
     api.on("select", onSelect)
 
     return () => {
+      api?.off("reinit", onSelect)
       api?.off("select", onSelect)
     }
   }, [api, onSelect])
